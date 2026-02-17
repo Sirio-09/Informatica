d5a34ce7a5bbf0ce8c2ae157714c7a34f40a05d5
Sirio <perronesirio@gmail.com>
2026-02-17 23:24:58 +0100

    Es Per 17/02/2026

---
+++ Matrici/Es 46 Pag 85/Es 46 Pag 85/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_46_Pag_85
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+        private static int LeggiNumero(string mess)
+        {
+            int n = 0;
+            bool isCorrect = false;
+
+            do
+            {
+                Console.WriteLine(mess);
+
+                int.TryParse(Console.ReadLine(), out n);
+
+                if (n > 0)
+                {
+                    isCorrect = true;
+                }
+
+            } while (!isCorrect);
+
+            return n;
+        }
+
+        private static void StampaMatrice(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool MatriceCrescente(int[,] matrix)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+
+                for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+                {
+
+                    if (matrix[row, col] >= matrix[row + 1, col])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static void RiempiMatriceCrescente(int[,] matrix)
+        {
+            int currentVal = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    currentVal += rnd.Next(1, 10);
+                    matrix[i, j] = currentVal;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int r = LeggiNumero("Inserisci il numero di righe: ");
+            int c = LeggiNumero("Inserisci il numero di colonne: ");
+
+            int[,] matrix = new int[r, c];
+            RiempiMatriceCrescente(matrix);
+
+            Console.WriteLine();
+            StampaMatrice(matrix);
+            Console.WriteLine();
+
+
+            if (MatriceCrescente(matrix))
+            {
+                Console.WriteLine("La matrice è in ordine crescente!");
+            }
+            else
+            {
+                Console.WriteLine("La matrice non è in ordine crescente!");
+            }
+        }
+    }
+}
---
+++ Matrici/Es 47 Pag 85/Es 47 Pag 85/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_47_Pag_85
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+        private static int LeggiNumero(string mess)
+        {
+            int n = 0;
+            bool isCorrect = false;
+
+            do
+            {
+                Console.WriteLine(mess);
+
+                int.TryParse(Console.ReadLine(), out n);
+
+                if (n > 0)
+                {
+                    isCorrect = true;
+                }
+
+            } while (!isCorrect);
+
+            return n;
+        }
+
+        private static void StampaMatrice(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool ConfrontaDiagonali(int[,] A, int[,] B)
+        {
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                if (A[i, i] != B[i, i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void RiempiMatrice(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 10);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int r = LeggiNumero("Inserisci l'ordine (r) delle matrici quadrate: ");
+
+            int[,] A = new int[r, r];
+            int[,] B = new int[r, r];
+
+            RiempiMatrice(A);
+            RiempiMatrice(B);
+
+            Console.WriteLine("\nMatrice A:");
+            StampaMatrice(A);
+
+            Console.WriteLine("\nMatrice B:");
+            StampaMatrice(B);
+
+            if (ConfrontaDiagonali(A, B))
+            {
+                Console.WriteLine("\nRisultato: Le diagonali principali sono UGUALI.");
+            }
+            else
+            {
+                Console.WriteLine("\nRisultato: Le diagonali principali sono DIVERSE.");
+            }
+        }
+    }
+}
+
