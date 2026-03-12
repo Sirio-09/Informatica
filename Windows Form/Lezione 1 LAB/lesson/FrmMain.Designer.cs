b331a8e570cd7cef7b7d05bfca26d7bc218f8a00
Sirio4104 <s.perrone.4104@vallauri.edu>
2026-03-12 10:24:48 +0100

    Aggiunta Lezione del 12/03/2026

---
+++ Windows Form/Lezione 1 LAB/lesson/FrmMain.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace lesson
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_partita = new System.Windows.Forms.Button();
+            this.lbl_error = new System.Windows.Forms.Label();
+            this.pcb_card9 = new System.Windows.Forms.PictureBox();
+            this.pcb_card8 = new System.Windows.Forms.PictureBox();
+            this.pcb_card7 = new System.Windows.Forms.PictureBox();
+            this.pcb_card4 = new System.Windows.Forms.PictureBox();
+            this.pcb_card5 = new System.Windows.Forms.PictureBox();
+            this.pcb_card6 = new System.Windows.Forms.PictureBox();
+            this.pcb_card3 = new System.Windows.Forms.PictureBox();
+            this.pcb_card2 = new System.Windows.Forms.PictureBox();
+            this.pcb_card1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_partita
+            // 
+            this.btn_partita.Location = new System.Drawing.Point(474, 36);
+            this.btn_partita.Name = "btn_partita";
+            this.btn_partita.Size = new System.Drawing.Size(108, 55);
+            this.btn_partita.TabIndex = 0;
+            this.btn_partita.Text = "NUOVA PARTITA";
+            this.btn_partita.UseVisualStyleBackColor = true;
+            this.btn_partita.Click += new System.EventHandler(this.btn_partita_Click);
+            // 
+            // lbl_error
+            // 
+            this.lbl_error.AutoSize = true;
+            this.lbl_error.Location = new System.Drawing.Point(474, 116);
+            this.lbl_error.Name = "lbl_error";
+            this.lbl_error.Size = new System.Drawing.Size(98, 15);
+            this.lbl_error.TabIndex = 1;
+            this.lbl_error.Text = "Errori Rimanenti: ";
+            this.lbl_error.Click += new System.EventHandler(this.lbl_error_Click);
+            // 
+            // pcb_card9
+            // 
+            this.pcb_card9.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card9.Location = new System.Drawing.Point(319, 398);
+            this.pcb_card9.Name = "pcb_card9";
+            this.pcb_card9.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card9.TabIndex = 10;
+            this.pcb_card9.TabStop = false;
+            this.pcb_card9.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card8
+            // 
+            this.pcb_card8.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card8.Location = new System.Drawing.Point(179, 398);
+            this.pcb_card8.Name = "pcb_card8";
+            this.pcb_card8.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card8.TabIndex = 9;
+            this.pcb_card8.TabStop = false;
+            this.pcb_card8.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card7
+            // 
+            this.pcb_card7.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card7.Location = new System.Drawing.Point(40, 398);
+            this.pcb_card7.Name = "pcb_card7";
+            this.pcb_card7.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card7.TabIndex = 8;
+            this.pcb_card7.TabStop = false;
+            this.pcb_card7.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card4
+            // 
+            this.pcb_card4.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card4.Location = new System.Drawing.Point(40, 218);
+            this.pcb_card4.Name = "pcb_card4";
+            this.pcb_card4.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card4.TabIndex = 5;
+            this.pcb_card4.TabStop = false;
+            this.pcb_card4.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card5
+            // 
+            this.pcb_card5.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card5.Location = new System.Drawing.Point(179, 218);
+            this.pcb_card5.Name = "pcb_card5";
+            this.pcb_card5.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card5.TabIndex = 6;
+            this.pcb_card5.TabStop = false;
+            this.pcb_card5.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card6
+            // 
+            this.pcb_card6.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card6.Location = new System.Drawing.Point(319, 218);
+            this.pcb_card6.Name = "pcb_card6";
+            this.pcb_card6.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card6.TabIndex = 7;
+            this.pcb_card6.TabStop = false;
+            this.pcb_card6.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card3
+            // 
+            this.pcb_card3.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card3.Location = new System.Drawing.Point(319, 36);
+            this.pcb_card3.Name = "pcb_card3";
+            this.pcb_card3.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card3.TabIndex = 4;
+            this.pcb_card3.TabStop = false;
+            this.pcb_card3.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card2
+            // 
+            this.pcb_card2.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card2.Location = new System.Drawing.Point(179, 36);
+            this.pcb_card2.Name = "pcb_card2";
+            this.pcb_card2.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card2.TabIndex = 3;
+            this.pcb_card2.TabStop = false;
+            this.pcb_card2.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // pcb_card1
+            // 
+            this.pcb_card1.BackColor = System.Drawing.SystemColors.Control;
+            this.pcb_card1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pcb_card1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcb_card1.Location = new System.Drawing.Point(40, 36);
+            this.pcb_card1.Name = "pcb_card1";
+            this.pcb_card1.Size = new System.Drawing.Size(119, 165);
+            this.pcb_card1.TabIndex = 2;
+            this.pcb_card1.TabStop = false;
+            this.pcb_card1.Click += new System.EventHandler(this.pcb_card_click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 594);
+            this.Controls.Add(this.pcb_card9);
+            this.Controls.Add(this.pcb_card8);
+            this.Controls.Add(this.pcb_card7);
+            this.Controls.Add(this.pcb_card6);
+            this.Controls.Add(this.pcb_card5);
+            this.Controls.Add(this.pcb_card4);
+            this.Controls.Add(this.pcb_card3);
+            this.Controls.Add(this.pcb_card2);
+            this.Controls.Add(this.pcb_card1);
+            this.Controls.Add(this.lbl_error);
+            this.Controls.Add(this.btn_partita);
+            this.Name = "FrmMain";
+            this.Text = "FrmMain";
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_card1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn_partita;
+        private Label lbl_error;
+        private PictureBox pcb_card9;
+        private PictureBox pcb_card8;
+        private PictureBox pcb_card7;
+        private PictureBox pcb_card4;
+        private PictureBox pcb_card5;
+        private PictureBox pcb_card6;
+        private PictureBox pcb_card3;
+        private PictureBox pcb_card2;
+        private PictureBox pcb_card1;
+    }
+}
