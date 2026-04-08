cca274354fe49a6a22416888fbe72a8b30a79802
Sirio4104 <s.perrone.4104@vallauri.edu>
2026-04-08 11:52:39 +0200

    Aggiunta Lezione del 08/04/2026

---
+++ Windows Form/Es Pag 168/Es Pag 168/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Es_Pag_168
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_scrivi = new System.Windows.Forms.Button();
+            this.btn_leggi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_scrivi
+            // 
+            this.btn_scrivi.Location = new System.Drawing.Point(43, 45);
+            this.btn_scrivi.Name = "btn_scrivi";
+            this.btn_scrivi.Size = new System.Drawing.Size(75, 33);
+            this.btn_scrivi.TabIndex = 0;
+            this.btn_scrivi.Text = "Scrivi File";
+            this.btn_scrivi.UseVisualStyleBackColor = true;
+            this.btn_scrivi.Click += new System.EventHandler(this.btn_scrivi_Click);
+            // 
+            // btn_leggi
+            // 
+            this.btn_leggi.Location = new System.Drawing.Point(143, 45);
+            this.btn_leggi.Name = "btn_leggi";
+            this.btn_leggi.Size = new System.Drawing.Size(75, 33);
+            this.btn_leggi.TabIndex = 1;
+            this.btn_leggi.Text = "Leggi File";
+            this.btn_leggi.UseVisualStyleBackColor = true;
+            this.btn_leggi.Click += new System.EventHandler(this.btn_leggi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 119);
+            this.Controls.Add(this.btn_leggi);
+            this.Controls.Add(this.btn_scrivi);
+            this.Name = "Form1";
+            this.Text = "Carica File";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_scrivi;
+        private System.Windows.Forms.Button btn_leggi;
+    }
+}
+
---
+++ Windows Form/Es Pag 168/Es Pag 168/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices;
+
+
+namespace Es_Pag_168
+{
+    struct Libri
+    {
+        public string codLibro;
+        public string titolo;
+        public string autore;
+        public string casaEditrice;
+        public string genere;
+        public string costo;
+
+        public override string ToString()
+        {
+            return $"{codLibro}, {titolo}, {autore}, {casaEditrice}, {genere}, {costo}";
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_scrivi_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("Libri.dat"))
+            {
+                Libri libro = new Libri();
+                string risposta = "";
+
+                do
+                {
+                    libro.codLibro = Interaction.InputBox("Inserisci il Codice: ");
+                    libro.titolo = Interaction.InputBox("Inserisci il Titolo: ");
+                    libro.autore = Interaction.InputBox("Inserisci l'Autore: ");
+                    libro.casaEditrice = Interaction.InputBox("Inserisci la Casa Editrice: ");
+                    libro.genere = Interaction.InputBox("Inserisci il Genere: ");
+                    libro.costo = Interaction.InputBox("Inserisci il Costo: ");
+
+                    sw.WriteLine(libro.ToString());
+
+                    risposta = Interaction.InputBox("Vuoi Continuare?(S/N)");
+
+                } while (risposta.ToUpper() == "S");
+            }
+        }
+
+        private void btn_leggi_Click(object sender, EventArgs e)
+        {
+            using(StreamReader sr = new StreamReader("Libri.dat"))
+            {
+                string riga = "";
+
+                while((riga = sr.ReadLine()) != null)
+                {
+                    string[] libro = new string[6];
+                    libro = riga.Split(',');
+                    string msg = "Dati del Libro: \n";
+
+                    foreach(string s in libro)
+                    {
+                        msg += s + "\n";
+                    }
+                    MessageBox.Show(msg);
+                }
+            }
+        }
+    }
+}
