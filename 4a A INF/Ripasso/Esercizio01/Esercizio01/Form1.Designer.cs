5fc17b28d8d6341e7045f755b0479ed0707d3e1e
Sirio <perronesirio@gmail.com>
2026-09-18 11:50:32 +0200

    lezione del 18/09

---
+++ 4a A INF/Ripasso/Esercizio01/Esercizio01/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Esercizio01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picVallauri = new System.Windows.Forms.PictureBox();
+            this.btnDuplica = new System.Windows.Forms.Button();
+            this.btnDuplica2 = new System.Windows.Forms.Button();
+            this.btnAggiungi = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picVallauri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picVallauri
+            // 
+            this.picVallauri.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picVallauri.Image = ((System.Drawing.Image)(resources.GetObject("picVallauri.Image")));
+            this.picVallauri.Location = new System.Drawing.Point(56, 42);
+            this.picVallauri.Name = "picVallauri";
+            this.picVallauri.Size = new System.Drawing.Size(202, 195);
+            this.picVallauri.TabIndex = 0;
+            this.picVallauri.TabStop = false;
+            // 
+            // btnDuplica
+            // 
+            this.btnDuplica.Location = new System.Drawing.Point(292, 42);
+            this.btnDuplica.Name = "btnDuplica";
+            this.btnDuplica.Size = new System.Drawing.Size(75, 23);
+            this.btnDuplica.TabIndex = 1;
+            this.btnDuplica.Text = "Duplica";
+            this.btnDuplica.UseVisualStyleBackColor = true;
+            this.btnDuplica.Click += new System.EventHandler(this.btnDuplica_Click);
+            // 
+            // btnDuplica2
+            // 
+            this.btnDuplica2.Location = new System.Drawing.Point(409, 43);
+            this.btnDuplica2.Name = "btnDuplica2";
+            this.btnDuplica2.Size = new System.Drawing.Size(75, 23);
+            this.btnDuplica2.TabIndex = 2;
+            this.btnDuplica2.Text = "Duplica2";
+            this.btnDuplica2.UseVisualStyleBackColor = true;
+            this.btnDuplica2.Click += new System.EventHandler(this.btnDuplica2_Click);
+            // 
+            // btnAggiungi
+            // 
+            this.btnAggiungi.Location = new System.Drawing.Point(528, 43);
+            this.btnAggiungi.Name = "btnAggiungi";
+            this.btnAggiungi.Size = new System.Drawing.Size(75, 23);
+            this.btnAggiungi.TabIndex = 3;
+            this.btnAggiungi.Text = "Aggiungi";
+            this.btnAggiungi.UseVisualStyleBackColor = true;
+            this.btnAggiungi.Click += new System.EventHandler(this.btnAggiungi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAggiungi);
+            this.Controls.Add(this.btnDuplica2);
+            this.Controls.Add(this.btnDuplica);
+            this.Controls.Add(this.picVallauri);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Esercizio 1 - Ripasso";
+            ((System.ComponentModel.ISupportInitialize)(this.picVallauri)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picVallauri;
+        private System.Windows.Forms.Button btnDuplica;
+        private System.Windows.Forms.Button btnDuplica2;
+        private System.Windows.Forms.Button btnAggiungi;
+    }
+}
+
---
+++ 4a A INF/Ripasso/Esercizio01/Esercizio01/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esercizio01
+{
+    public partial class Form1 : Form
+    {
+
+        int Diff = 20;
+        int prgImmagine = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDuplica_Click(object sender, EventArgs e)
+        {
+           
+            
+            PictureBox nuovaIMG = new PictureBox();
+
+            nuovaIMG.Size = new Size(picVallauri.Width, picVallauri.Height);
+
+            /// IMPORTANTE !!!
+            this.Controls.Add(nuovaIMG);
+
+            nuovaIMG.Image = picVallauri.Image;
+            nuovaIMG.Name = $"picDuplica{prgImmagine++}";
+
+            nuovaIMG.Location = new Point(picVallauri.Location.X + Diff, picVallauri.Location.Y + Diff);
+
+            Diff += 20;
+        }
+
+        private void btnDuplica2_Click(object sender, EventArgs e)
+        {
+
+            PictureBox nuovaIMG = new PictureBox();
+            nuovaIMG.Size = new Size(200, 100);
+
+            /// IMPORTANTE !!!
+            this.Controls.Add(nuovaIMG);
+
+            Bitmap bmp = new Bitmap(200, 100);
+            Graphics bmpGraphics = Graphics.FromImage(bmp);
+
+            bmpGraphics.FillRectangle(Brushes.Blue, 0, 0, 200, 100);
+            nuovaIMG.Image = bmp;
+
+            nuovaIMG.Name = $"picDuplica2{prgImmagine++}";
+
+            nuovaIMG.Location = new Point(picVallauri.Location.X + Diff, picVallauri.Location.Y + Diff);
+
+            Diff += 50;
+        }
+
+        private void btnAggiungi_Click(object sender, EventArgs e)
+        {
+            TextBox nuovoTXT = new TextBox();
+
+            nuovoTXT.Size = new Size(200, 23);
+            nuovoTXT.Name = "txtInput";
+
+            this.Controls.Add(nuovoTXT);
+
+            nuovoTXT.Location = new Point(btnDuplica.Location.X,
+                                            btnDuplica.Location.Y +
+                                            btnDuplica.Size.Height + 25);
+
+            Button nuouvoBTN = new Button()
+            {
+                Size = new Size(btnAggiungi.Size.Width, btnAggiungi.Size.Height),
+                Name = "btnVisualizza",
+                Text = "Visualizza",
+                Location = new Point(btnAggiungi.Location.X,
+                                    btnAggiungi.Location.Y +
+                                    btnAggiungi.Size.Height + 25)
+            };
+
+            // Evento CLICK
+            nuouvoBTN.Click += btnVisualizza_Click;
+
+            this.Controls.Add(nuouvoBTN);
+
+            btnAggiungi.Enabled = false;
+
+        }
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            Control control = sender as Control;
+
+            MessageBox.Show($"Evento creato da [{control.Name}] : {Controls["txtInput"].Text}");
+        }
+
+    }
+}
